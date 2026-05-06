90e8dd996088df3b17da0bb45e1253a4d08de747
zinoos <zinos.dao@gmail.com>
2026-05-06 09:10:39 +0200

    Implemented GUI

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,5 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
 
 public class Starter {
     public static void main(String[] args) {
@@ -8,9 +8,20 @@ public class Starter {
 
     private static void createAndShowGui() {
         JFrame frame = new JFrame("Snack Automat");
-        frame.setSize(500, 500);
+        frame.setSize(1280, 538);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+
+        frame.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+
+        ImageIcon icon = new ImageIcon("C:\\Users\\zinob\\Documents\\Snack_Automat\\assets\\automat.png");
+        Image image = icon.getImage();
+
+        Image scaledImage = image.getScaledInstance(312, 538, Image.SCALE_SMOOTH);
+        JLabel backgroundLabel = new JLabel(new ImageIcon(scaledImage));
+
+        frame.add(backgroundLabel);
+
         frame.setVisible(true);
     }
 }
